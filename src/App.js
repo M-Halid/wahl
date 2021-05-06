1ef2d8f9678239f3aa852c7030089883ade4bb4a
M-Halid <59921239+M-Halid@users.noreply.github.com>
2021-05-06 11:19:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+        <h5>
           allright not on gh but Netlify ! 😅
-        </h1>
+        </h5>
         <a
           className="App-link"
           href="https://reactjs.org"
