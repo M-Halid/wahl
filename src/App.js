de3a3c714ac09789b69c434941b197c544427476
M-Halid <59921239+M-Halid@users.noreply.github.com>
2021-05-06 11:06:37 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          am i on GH ?
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+//npm set strict-ssl false
